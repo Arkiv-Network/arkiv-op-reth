c1934d8db19101fb666d67cf21958c2eccabbe52
scx1332 <sieciech@gmail.com>
2026-04-30 16:05:42 +0200

    Working on fd-agentic tasks support

---
+++ .fdagent/fd-agent.md
@@ -0,0 +1,4 @@
+** Do not commit this file to repo, these are only overwritten for local development. **
+
+
+
---
+++ .fdagent/setup.sh
@@ -0,0 +1,7 @@
+
+# Remove complicated instruction to let CODEX discover everything by himself
+rm AGENTS.md
+cp fd-agent.md AGENTS.md
+
+
+
