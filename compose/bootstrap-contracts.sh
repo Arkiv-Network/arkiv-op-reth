fbf86d2d12c49bf6b6b404878beec0575981a47a
scx1332 <sieciech@gmail.com>
2026-05-01 00:07:25 +0200

    Working on docker e2e

--- compose/bootstrap-contracts.sh
+++ compose/bootstrap-contracts.sh
@@ -2,6 +2,8 @@
 
 set -xeuo pipefail
 
+ARKIV_NODE_IMAGE="${ARKIV_NODE_IMAGE:-ghcr.io/arkiv-network/arkiv-node}"
+
 cp ../chainspec/dev.base.json genesis.json && sudo chown 999:999 genesis.json
 sudo rm -fr ./reth ./storaged
 
@@ -13,12 +15,12 @@ docker run --rm \
   -v ./reth:/home/docker/.local/share/reth \
   -v ./storaged:/home/docker/.arkiv-storaged \
   --entrypoint arkiv-cli \
-  ghcr.io/arkiv-network/arkiv-node \
+  "$ARKIV_NODE_IMAGE" \
   inject-predeploy /home/docker/genesis.json
 
 docker run --rm \
   -v ./genesis.json:/home/docker/genesis.json \
   -v ./reth:/home/docker/.local/share/reth \
   -v ./storaged:/home/docker/.arkiv-storaged \
-  ghcr.io/arkiv-network/arkiv-node \
+  "$ARKIV_NODE_IMAGE" \
   init --chain /home/docker/genesis.json
