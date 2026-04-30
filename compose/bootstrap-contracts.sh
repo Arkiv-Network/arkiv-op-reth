6975e10c52e1e5be5d22a833b060c99b5c1a8923
scx1332 <sieciech@gmail.com>
2026-04-30 23:43:24 +0200

    Working on docker e2e

---
+++ compose/bootstrap-contracts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cp ../chainspec/dev.base.json genesis.json
+docker run -d \
+  --name arkiv-node \
+  -v ./genesis.json:/home/docker/genesis.json \
+  -v ./reth:/home/docker/.local/share/reth \
+  -v ./storaged:/home/docker/.arkiv-storaged \
+  --entrypoint arkiv-cli \
+  ghcr.io/arkiv-network/arkiv-node \
+  inject-predeploy /home/docker/genesis.json
+
+docker run -d \
+  --name arkiv-node \
+  -v ./genesis.json:/home/docker/genesis.json \
+  -v ./reth:/home/docker/.local/share/reth \
+  -v ./storaged:/home/docker/.arkiv-storaged \
+  ghcr.io/arkiv-network/arkiv-node \
+  init --chain /home/docker/genesis.json
